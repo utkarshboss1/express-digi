2abcd185c70e164a63ecee2f2f05d5d199f29fe3
utkarshboss1 <utkarsh.baj3.14@gmail.com>
2026-08-26 19:18:27 +0530

    Add dotenv package and update .gitignore for environment variables

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+import 'dotenv/config'
 import express from "express";
 
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT || 3000;
 /*
 app.get("/", (req, res) => {
   res.send("Hello from Hitesh and his tea!");
